4f41d87dfe2ddcf6f097c6b616503d37776cd7ad
Mateusz <wszeborowskimateusz@gmail.com>
2019-05-11 17:27:45 +0200

    By default redirect to '/'

--- src/router.js
+++ src/router.js
@@ -8,11 +8,7 @@ const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
+    { path: '/', name: 'home', component: Home },
     {
       path: '/about',
       name: 'about',
@@ -21,6 +17,7 @@ const router = new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    { path: '*', redirect: '/' },
   ],
 });
 
