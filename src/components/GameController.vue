b4e302d159700de6af7f04c4b670385bda6174ce
Mateusz <wszeborowskimateusz@gmail.com>
2019-06-08 13:04:26 +0200

    Adding backgroud image to game controller

--- src/components/GameController.vue
+++ src/components/GameController.vue
@@ -1,42 +1,67 @@
+<!--:style="{'background-image' : 'url(' + categoryBackgroundImage +')'}" -->
 <template>
     <div class="GameController col-12">
-        <h3 class="title">This is categorie with id = {{$route.params.id}}</h3>
-        <div class="progress m-2">
-                <div class="progress-bar dynamic progress-bar-animated progress-bar-striped"
-                    :style="{ width: this.gameProgress + '%' }"
-                    role="progressbar" :aria-valuenow="this.gameProgress"
-                    aria-valuemin="0" aria-valuemax="100"></div>
-                <div class="progress-bar-title bar-title">
-                    {{this.currentGameIndex + 1}} / {{this.games.length}}
-                </div>
+        <div class="background-image"
+            :style="{'background-image' : 'url(' + categoryBackgroundImage +')'}">
         </div>
-        <component class="game"
-            v-bind:gameInfo="games[currentGameIndex].gameInfo"
-            :is="games[currentGameIndex].name"></component>
-        <ul class="pagination justify-content-center">
-            <li class="page-item pr-5 pt-3">
-                <button v-on:click="prevGame()"
-                    class="page-link">Poprzedni</button>
-            </li>
-            <li class="page-item pl-5 pt-3">
-                <button v-on:click="nextGame()"
-                    class="page-link">Następny</button>
-            </li>
-        </ul>
+        <div class="content">
+            <h3 class="title">This is categorie with id = {{$route.params.id}}</h3>
+            <div class="progress m-2">
+                    <div class="progress-bar dynamic progress-bar-animated progress-bar-striped"
+                        :style="{ width: this.gameProgress + '%' }"
+                        role="progressbar" :aria-valuenow="this.gameProgress"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar-title bar-title">
+                        {{this.currentGameIndex + 1}} / {{this.games.length}}
+                    </div>
+            </div>
+            <div class="gameContainer">
+                <component class="game"
+                    v-bind:gameInfo="games[currentGameIndex].gameInfo"
+                    :is="games[currentGameIndex].name">
+                </component>
+            </div>
+            <div class="btn-toolbar mb-12 justify-content-center"
+                role="toolbar" aria-label="Toolbar with button groups">
+                <div class="btn-group mr-12 p-1" role="group" aria-label="First group">
+                    <button type="button" class="m-3 btn btn-primary"
+                        v-on:click="prevGame()">Poprzedni</button>
+                    <button type="button" class="m-3 btn btn-primary"
+                        v-on:click="nextGame()">Następny</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
 .GameController {
-     text-align: center;
+    text-align: center;
+    color: #000;
 }
 .title {
     height: 5vh;
 }
 
+.background-image {
+    filter: blur(5px);
+    -webkit-filter: blur(5px);
+    background-position: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    background-size:cover;
+    width: 100%;
+    height: 100%;
+}
+
 .game {
     height: 65vh;
     padding: 1%;
+    background: rgba(244, 229, 221, 0.5);
 }
 
 .progress {
@@ -94,6 +119,7 @@ export default {
         },
       ],
       currentGameIndex: 0,
+      categoryBackgroundImage: 'https://wallup.net/wp-content/uploads/2018/10/09/1036532-animals-background-53.jpg',
     };
   },
   computed: {
