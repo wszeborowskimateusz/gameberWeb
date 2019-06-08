9b0f3585f76bc5d4a3fc167b3216f487ee0cf8bb
Mateusz <wszeborowskimateusz@gmail.com>
2019-06-08 11:33:50 +0200

    Adding progress bar to game controller

--- src/components/GameController.vue
+++ src/components/GameController.vue
@@ -1,15 +1,25 @@
 <template>
     <div class="GameController col-12">
         <h3 class="title">This is categorie with id = {{$route.params.id}}</h3>
-        <component class="game" v-bind:gameInfo="this.gameInfo"
-            :is="currentGameComponent"></component>
+        <div class="progress m-2">
+                <div class="progress-bar dynamic progress-bar-animated progress-bar-striped"
+                    :style="{ width: this.gameProgress + '%' }"
+                    role="progressbar" :aria-valuenow="this.gameProgress"
+                    aria-valuemin="0" aria-valuemax="100"></div>
+                <div class="progress-bar-title bar-title">
+                    {{this.currentGameIndex + 1}} / {{this.games.length}}
+                </div>
+        </div>
+        <component class="game"
+            v-bind:gameInfo="games[currentGameIndex].gameInfo"
+            :is="games[currentGameIndex].name"></component>
         <ul class="pagination justify-content-center">
             <li class="page-item pr-5 pt-3">
-                <button v-on:click="changeComponent('WordGet')"
+                <button v-on:click="prevGame()"
                     class="page-link">Poprzedni</button>
             </li>
             <li class="page-item pl-5 pt-3">
-                <button v-on:click="changeComponent('WordLearning')"
+                <button v-on:click="nextGame()"
                     class="page-link">Następny</button>
             </li>
         </ul>
@@ -25,7 +35,22 @@
 }
 
 .game {
-    height: 70vh;
+    height: 65vh;
+}
+
+.progress {
+  position: relative;
+}
+
+.progress-bar-title {
+  position: absolute;
+  text-align: center;
+  line-height: 20px;
+  overflow: hidden;
+  color: #000;
+  right: 0;
+  left: 0;
+  top: 0;
 }
 
 </style>
@@ -37,22 +62,58 @@ import WordGet from './Games/WordGet.vue';
 export default {
   data() {
     return {
-      currentGameComponent: 'WordLearning',
-      gameInfo: {
-        id: 'some id',
-        info: 'this is some info',
-      },
+      games: [
+        {
+          name: 'WordLearning',
+          gameInfo: {
+            id: '1',
+            info: 'this is some info',
+          },
+        },
+        {
+          name: 'WordLearning',
+          gameInfo: {
+            id: '2',
+            info: 'this is some info',
+          },
+        },
+        {
+          name: 'WordGet',
+          gameInfo: {
+            id: '3',
+            info: 'this is some info',
+          },
+        },
+        {
+          name: 'WordLearning',
+          gameInfo: {
+            id: '4',
+            info: 'this is some info',
+          },
+        },
+      ],
+      currentGameIndex: 0,
     };
+  },
+  computed: {
+    gameProgress() {
+      return (this.currentGameIndex + 1) / this.games.length * 100;
+    },
   },
   components: {
     WordLearning,
     WordGet,
   },
   methods: {
-    changeComponent(name) {
-      this.currentGameComponent = name;
-      this.gameInfo.id = name;
-      this.gameInfo.info = `This is info for ${name}`;
+    nextGame() {
+      if (this.currentGameIndex + 1 < this.games.length) {
+        this.currentGameIndex += 1;
+      }
+    },
+    prevGame() {
+      if (this.currentGameIndex - 1 >= 0) {
+        this.currentGameIndex -= 1;
+      }
     },
   },
 };
