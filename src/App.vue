d2c8c9afb61de2c96fdaae67170d835087d4f7f1
Mateusz <wszeborowskimateusz@gmail.com>
2019-07-01 20:53:51 +0200

    Changing name of the link to Ranking

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
             <router-link class="nav-link" to="/about">O nas</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/ranking">Ranking graczy</router-link>
+            <router-link class="nav-link" to="/ranking">Ranking</router-link>
           </li>
           <li v-if="status.loggedIn" class="nav-item">
               <router-link class="nav-link" to="/map">Mapa</router-link>
