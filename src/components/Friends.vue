8bad79dce563ceefcf32bc654fb0c53563b93cea
Mateusz <wszeborowskimateusz@gmail.com>
2019-08-10 14:08:44 +0200

    Fetching friends from server

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="col-12 friends__container row" v-if="friends != null && friends.length > 0">
+  <div v-if="isLoading" class="col-12 d-flex justify-content-center">
+    <cube-spin class="m-2"></cube-spin>
+  </div>
+  <div class="col-12 friends__container row" v-else-if="friends != null && friends.length > 0">
     <p class="h1 col-12">Znajomi</p>
     <div v-for="friend in friends" class="friends__row row" v-bind:key="friend.id">
       <div class="col-12 store__item m-1 mb-3" :class="['col-lg-3', 'col-md-6', 'col-sm-6']">
@@ -60,30 +63,37 @@ figcaption {
 </style>
 
 <script>
+import { mapState } from 'vuex';
+import CubeSpin from 'vue-loading-spinner/src/components/Circle8.vue';
+import friendsService from '../services/friendsService';
+
 export default {
   data() {
     return {
-      friends: [
-        {
-          id: 1,
-          name: 'Jon Snow',
-          avatar:
-            'https://cdn.vox-cdn.com/thumbor/o2AXRjdoyonKroOEsxQjYWvtG-U=/99x0:1179x810/1200x800/filters:focal(99x0:1179x810)/cdn.vox-cdn.com/uploads/chorus_image/image/46094226/Jon_snow.0.jpg',
-        },
-        {
-          id: 2,
-          name: 'Will Byers',
-          avatar:
-            'https://vignette.wikia.nocookie.net/strangerthings8338/images/e/e4/Will_Byers.png/revision/latest/scale-to-width-down/310?cb=20170124225951',
-        },
-        {
-          id: 3,
-          name: 'Nancy Wheeler',
-          avatar:
-            'https://vignette.wikia.nocookie.net/strangerthings8338/images/2/22/Nancy_S3.png/revision/latest?cb=20190704211903',
-        },
-      ],
+      isLoading: false,
+      friends: [],
     };
+  },
+  computed: {
+    ...mapState('users', ['user']),
+  },
+  created() {
+    this.isLoading = true;
+    this.fetchFriends();
+  },
+  methods: {
+    fetchFriends() {
+      friendsService
+        .getFriends(this.user)
+        .then((friends) => {
+          this.isLoading = false;
+          this.friends = friends;
+        })
+        .then(() => this.$forceUpdate());
+    },
+  },
+  components: {
+    CubeSpin,
   },
 };
 </script>
