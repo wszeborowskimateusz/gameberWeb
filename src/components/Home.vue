56b3b42c251e7fab68b15437f68b15f00329ac04
Mateusz <wszeborowskimateusz@gmail.com>
2019-08-14 19:40:47 +0200

    Adding home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,38 @@
 <template>
-    <div class="home">
-
+  <div class="home col-12">
+    <p class="h1 mb-5">GAMEBER = GAME + REMEMBER</p>
+    <img
+      width="250"
+      src="https://www.isebrighton.com/userfiles/image/expertbeginner.png"
+      class="rounded img-thumbnail"
+    />
+    <div class="shadow-lg m-5 pt-3 pb-5">
+      <p class="m-5 h5">
+        Jeżeli chcesz się nauczyć angielskiego
+        i przy okazji dobrze się bawić, to ta aplikacja jest dla ciebie
+      </p>
+      <p class="h4">
+        Jeżeli chcesz się przekonać jak wygląda aplikacja, zajrzyj do
+        <router-link to="/tutorial">tutoriala</router-link>
+      </p>
     </div>
+    <img
+      class="img-fluid"
+      src="https://img.pngio.com/english-is-fun-png-english-is-fun-2000_1158.png"
+    />
+    <h3 class="m-3">Gameber to:</h3>
+    <ul class="list-group mb-5" id="features-list">
+      <li class="list-group-item">
+        <i class="fas fa-smile-beam float-left"></i> Świetna zabawa
+      </li>
+      <li class="list-group-item">
+        <i class="fas fa-book float-left"></i> Możliwość nauki
+      </li>
+      <li class="list-group-item">
+        <i class="fas fa-gamepad float-left"></i> Rywalizacja
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,46 +1,23 @@
 <template>
   <div class="about col-12">
     <h1 class="mb-5">Gameber - Game + Remember</h1>
-
-    <div class="row">
-      <div class="col-6">
-        <h3 class="mb-3">Gameber to:</h3>
-        <ul class="list-group" id="features-list">
-          <li class="list-group-item">
-            <i class="fas fa-smile-beam float-left"></i> Świetna zabawa</li>
-          <li class="list-group-item"><i class="fas fa-book float-left"></i> Możliwość nauki</li>
-          <li class="list-group-item"><i class="fas fa-gamepad float-left"></i> Rywalizacja</li>
-        </ul>
-      </div>
-      <div class="col-6">
-        <h3 class="mb-3">Autorzy:</h3>
-        <ul class="list-group" id="features-list">
-          <li class="list-group-item">
-            <a href="https://github.com/wszeborowskimateusz" target="_blank">
-            Mateusz Wszeborowski
-            </a>
-          </li>
-          <li class="list-group-item">
-            <a href="https://github.com/kkarbowski" target="_blank">
-              Korneliusz Karbowski
-            </a>
-          </li>
-          <li class="list-group-item">
-            <a href="https://github.com/maciejgrzywacz" target="_blank">
-            Maciej Grzywacz
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-
+    <h3 class="mb-3">Autorzy:</h3>
+    <ul class="list-group" id="features-list">
+      <li class="list-group-item">
+        <a href="https://github.com/wszeborowskimateusz" target="_blank">Mateusz Wszeborowski</a>
+      </li>
+      <li class="list-group-item">
+        <a href="https://github.com/kkarbowski" target="_blank">Korneliusz Karbowski</a>
+      </li>
+      <li class="list-group-item">
+        <a href="https://github.com/maciejgrzywacz" target="_blank">Maciej Grzywacz</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
-
 .about {
   text-align: center;
 }
-
 </style>
