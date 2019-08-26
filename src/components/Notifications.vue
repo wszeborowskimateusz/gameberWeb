0d2315803e819f984c93203ae66b093000f9b396
Mateusz <wszeborowskimateusz@gmail.com>
2019-08-26 20:42:44 +0200

    Fixing problematic notifications

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -182,15 +182,19 @@ export default {
   computed: {
     ...mapState('notificationsStore', ['notifications', 'isLoading']),
     amountOfReadNotifications() {
+      if (this.notifications === undefined) { return []; }
       return this.notifications.filter(x => x.isRead).length;
     },
     amountOfUnReadNotifications() {
+      if (this.notifications === undefined) { return []; }
       return this.notifications.filter(x => !x.isRead).length;
     },
     ReadNotifications() {
+      if (this.notifications === undefined) { return []; }
       return this.notifications.filter(x => x.isRead);
     },
     UnReadNotifications() {
+      if (this.notifications === undefined) { return []; }
       return this.notifications.filter(x => !x.isRead);
     },
     NotificationsStatus() {
