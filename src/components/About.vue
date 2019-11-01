189f27db0263266554a49179f4c5629c6a8b38ef
Mateusz <wszeborowskimateusz@gmail.com>
2019-11-01 10:54:24 +0100

    Adding mail address to a contact page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about col-12">
-    <h1 class="mb-5">Gameber - Game + Remember</h1>
+    <h1 class="mb-5 mt-2">Gameber = Game + Remember</h1>
     <h3 class="mb-3">Autorzy:</h3>
     <ul class="list-group" id="features-list">
       <li class="list-group-item">
@@ -13,6 +13,13 @@
         <a href="https://github.com/maciejgrzywacz" target="_blank">Maciej Grzywacz</a>
       </li>
     </ul>
+    <h5 class="m-3">
+      Jeżeli chcesz nam coś przekazać, wyślij nam email na
+      <a href="mailto://gameberproject.bugreport@gmail.com">
+        gameberproject.bugreport@gmail.com
+      </a>
+      lub skorzystaj z formularza poniżej
+    </h5>
     <h2 class="p-3">Skontaktuj się z nami</h2>
     <ContactForm class="mt-3 mb-2"/>
   </div>
